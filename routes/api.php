973b38319fa5962c354eb97cea9ebf9a878a8a3d
Mr-Jhay <gumabonjayar6@gmail.com>
2024-08-24 13:02:21 +0800

    main

--- routes/api.php
+++ routes/api.php
@@ -80,7 +80,7 @@ Route::group([
     Route::get('viewAllStudents', [UsersController::class, 'viewAllStudents']);
     Route::put('updateStudent', [UsersController::class, 'updateStudent']);
 
-    Route::put('updateUserPassword/{user}', [UsersController::class, 'updateUserPassword']);//both teacher and student
+    Route::put('/user/{user}/update-password', [UsersController::class, 'updateUserPassword']);//both teacher and student
     Route::put('updateOwnPassword', [UsersController::class, 'updateOwnPassword']);
     
     Route::get('viewallusers', [UsersController::class, 'viewallusers']);
