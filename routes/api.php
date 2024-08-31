292df83ea7d5c3385b78dcc5e496d5d6c5e0e93f
Mr-Jhay <gumabonjayar6@gmail.com>
2024-08-31 21:40:59 +0800

    main

--- routes/api.php
+++ routes/api.php
@@ -142,7 +142,7 @@ Route::group([
     Route::get('listStudentsInClass2', [joinclassController::class, 'listStudentsInClass2']);//list of Students In Class 1 accepted may previlage
     Route::get('listStudentsInClass3', [joinclassController::class, 'listStudentsInClass3']);//list of Students In Class 0 pending 
 
-    Route::post('approveStudentJoinRequest', [joinclassController::class, 'approveStudentJoinRequest']);
+    
 
 
    
