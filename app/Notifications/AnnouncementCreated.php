2b2e85fb7077aa57b4cf27870158661e59ad49e8
Mr-Jhay <gumabonjayar6@gmail.com>
2024-09-04 23:57:41 +0800

    main

---
+++ app/Notifications/AnnouncementCreated.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+use App\Models\Announcement;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AnnouncementCreated extends Notification
+{
+    use Queueable;
+
+    private Announcement $annoucement;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Announcement $annoucement)
+    {
+        $this->announcement = $announcement;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                     ->greeting($this->announcement->title)
+                    ->line($this->announcement->description)
+                    ->action('Go to app', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
