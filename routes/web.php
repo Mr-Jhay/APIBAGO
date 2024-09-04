b131a99e3b7764c0ec4bd69382fd3c468b914ca0
Mr-Jhay <gumabonjayar6@gmail.com>
2024-09-04 15:04:31 +0800

    main

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\smsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/send-sms', [smsController::class, 'sendSms']);
